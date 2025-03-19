b52dd0698ccae39be75550179bc200d3175e035b
phucdt <dotienphuc1607@gmail.com>
2025-03-19 11:04:12 +0700

    Update main.js

--- Session 18/Bai4/main.js
+++ Session 18/Bai4/main.js
@@ -1,32 +1,33 @@
 let n = Number(prompt("Nhập số: "));
 
-if (n === 1) {
-    document.write("Số một");
-}
-else if (n === 2) {
-    document.write("Số hai");
-}
-else if (n === 3) {
-    document.write("Số ba");
-}
-else if (n === 4) {
-    document.write("Số bốn");
-}
-else if (n === 5) {
-    document.write("Số năm");
-}
-else if (n === 6) {
-    document.write("Số sáu");
-}
-else if (n === 7) {
-    document.write("Số bảy");
-}
-else if (n === 8) {
-    document.write("Số tám");
-}
-else if (n === 9) {
-    document.write("Số chín");
-}
-else {
-    document.write("Nhập 1 đến 9 thoi");
+switch (n) {
+    case 1:
+        document.write("Số một");
+        break;
+    case 2:
+        document.write("Số hai");
+        break;
+    case 3:
+        document.write("Số ba");
+        break;
+    case 4:
+        document.write("Số bốn");
+        break;
+    case 5:
+        document.write("Số năm");
+        break;
+    case 6:
+        document.write("Số sáu");
+        break;
+    case 7:
+        document.write("Số bảy");
+        break;
+    case 8:
+        document.write("Số tám");
+        break;
+    case 9:
+        document.write("Số chín");
+        break;
+    default:
+        document.write("Nhập 1 đến 9 thôi");
 }
